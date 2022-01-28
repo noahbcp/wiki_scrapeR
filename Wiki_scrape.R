01451a61e35880821a7073cebe9fda809b5d968f
noahbcp <noah_piper@hotmail.com>
2022-01-28 18:12:47 +1000

    Add annotations.

--- Wiki_scrape.R
+++ Wiki_scrape.R
@@ -4,14 +4,15 @@
     library(lubridate)
     library(htmltools)
     library(rvest)
-#get user input
+
+#Get user input
     get_name <- as.character(readline(prompt = 'Please enter your name: '))
     get_age <- as.integer(readline(prompt = 'Please enter your age: '))
     get_bday <- as.Date(readline(prompt = 'Please enter your birthday (dd/mm/yy): '), format = '%d/%m/%y')
-#calculate time diff between systime and user birthday
+#Calculate time diff between systime and user birthday
     next_bday <- as.Date(get_bday %m+% years(get_age + 1))
     get_systime <- as.Date(Sys.time())
-#convert days, hours, min, sec to integers and print
+#Convert days, hours, min, sec to integers and print
     daysuntil <- as.integer(next_bday - get_systime)
     hoursuntil <- as.integer(daysuntil * 24)
     minutesuntil <- as.integer(hoursuntil * 60)
@@ -20,16 +21,22 @@
     print(paste("That's ", hoursuntil, 'hours away...'))
     print(paste("or, ", minutesuntil, 'minutes away...'))
     print(paste("or, a whopping,", secondsuntil, 'seconds away!'))
-#get user input
+#Get user input for legibility reasons
     readline(prompt = 'Press enter to continue...')
-#get Wikipedia 'on this day'
-    wikiurldate <- (paste(months(get_bday), '_', day(get_bday), sep = ''))
-    wikiurl <- paste('https://en.wikipedia.org/wiki/Wikipedia:Selected_anniversaries/', wikiurldate, sep = '')
-    content <- wikiurl %>%
-            read_html() %>%
-            html_node(xpath = '//*[@id="mw-content-text"]/div[1]/ul') %>%
+#Get Wikipedia 'on this day'
+    wikiurldate <- (paste(months(get_bday), '_', day(get_bday), sep = '')) 
+        #url requires Month_D format
+    wikiurl <- paste('https://en.wikipedia.org/wiki/Wikipedia:Selected_anniversaries/', wikiurldate, sep = '') #appends date
+    content <- wikiurl %>% #%>% passes the vector on to each following command
+            read_html() %>% 
+                #Pulls html from wikiurl
+            html_node(xpath = '//*[@id="mw-content-text"]/div[1]/ul') %>% 
+                #Denotes where to 'look.' 
+                #Specifically, references the 'past front page' section.
             html_children() %>%
+                #Gets the html children
             html_text2()
+                #Cleans up html formatting from text
     print(paste('Did you know that on the ', day(get_bday), ' ', months(get_bday), ' the following happened...', sep = ''))
     noquote(content)
 }
