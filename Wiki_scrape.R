56094c6b1b46323e361319dac07c45b15f8f196f
noahbcp <noah_piper@hotmail.com>
2022-01-31 10:31:35 +1000

    Cleaned syntax. Removed superflous calculator.

--- Wiki_scrape.R
+++ Wiki_scrape.R
@@ -1,78 +1,68 @@
 {
-#Dependencies
+##Dependencies
     library(lubridate)
     library(htmltools)
     library(rvest)
-
-#Get user input
-    get_name <- as.character(readline(prompt = 'Please enter your name: '))
-    get_age <- as.integer(readline(prompt = 'Please enter your age: '))
-    get_bday <- as.Date(readline(prompt = 'Please enter your birthday (dd/mm/yy): '), format = '%d/%m/%y')
-#Calculate time diff between systime and user birthday
-    next_bday <- as.Date(get_bday %m+% years(get_age + 1))
-    get_systime <- as.Date(Sys.time())
-#Convert days, hours, min, sec to integers and print
-    daysuntil <- as.integer(next_bday - get_systime)
-    hoursuntil <- as.integer(daysuntil * 24)
-    minutesuntil <- as.integer(hoursuntil * 60)
-    secondsuntil <- as.integer(minutesuntil * 60)
-    print(paste('Hi', paste(get_name, '!', sep = ''), 'It is', daysuntil, 'days until your next birthday!'))
-    print(paste("That's ", hoursuntil, 'hours away...'))
-    print(paste("or, ", minutesuntil, 'minutes away...'))
-    print(paste("or, a whopping,", secondsuntil, 'seconds away!'))
-#Get user input for legibility reasons
-    readline(prompt = 'Press enter to continue...')
-#Get Wikipedia 'on this day'
-    wikiurldate <- (paste(months(get_bday), '_', day(get_bday), sep = '')) 
-        #url requires Month_D format
-    wikiurl <- paste('https://en.wikipedia.org/wiki/Wikipedia:Selected_anniversaries/', wikiurldate, sep = '') #appends date
-    content <- wikiurl %>% #%>% passes the vector on to each following command
-            read_html() %>% 
-                #Pulls html from wikiurl
+##Get user name and birthday
+    get_name <- as.character(readline(
+                prompt = 'Please enter your name: ')
+                )
+    get_bday <- as.Date(readline(
+                prompt = 'Please enter your birthday (dd/mm/yy): '), 
+                format = '%d/%m/%y'
+                )
+##Scrape Wikipedia 'on this day' page
+    wikiurl_date <- (paste(months(get_bday), '_', day(get_bday), sep = '')) 
+    wikiurl <-  paste(
+                'https://en.wikipedia.org/wiki/Wikipedia:Selected_anniversaries/', 
+                wikiurl_date, sep = ''
+                )
+    content <- wikiurl %>%
+            read_html() %>%
             html_node(xpath = '//*[@id="mw-content-text"]/div[1]/ul') %>% 
-                #Denotes where to 'look.' 
-                #Specifically, references the 'past front page' section.
-            html_children() %>%
-                #Converts the {html_node} into a list
-            html_text2()
-                #Cleans up html formatting from text
-    print(paste('Did you know that on the ', day(get_bday), ' ', months(get_bday), ' the following happened...', sep = ''))
+            html_children() %>%     #Converts the {html_node} into a list
+            html_text2()    #Cleans up html formatting from text
+    dyk_prompt <-   as.character(paste(
+                    'Hi, ', get_name, '!', ' Did you know that on the ', 
+                    day(get_bday), ' ', months(get_bday), 
+                    ' the following happened...', ' [press enter]', sep = '')
+                    )
+    readline(dyk_prompt)
     rand_fact <- noquote(sample(content, 1, replace = FALSE))
     print(rand_fact)
-    i <- match(rand_fact, content) #finds rand_fact in the list and removes it to prevent double-ups
-    content <- content[-i]
-#Samples the content list; returns a single item
-
-
-#Loops and returns a new (*not yet*) random fact.
-        q_response <- as.character(readline(prompt = 'Do you want to hear another fact? (Y/N): '))
-        while (q_response == 'Y' & length(content) > 0)
-            {
-            rand_fact <- noquote(sample(content, 1, replace = FALSE))
-            print(rand_fact)
-            i <- match(rand_fact, content)
-            content <- content[-i]
-            q_response <- NULL
-            q_response <- as.character(readline(prompt = 'Do you want to hear another fact? (Y/N): '))
+    i <- match(rand_fact, content) #find printed rand_fact in the list
+    content <- content[-i] #removes the printed rand_fact
+    q_response <-   as.character(readline(
+                    prompt = 'Do you want to hear another fact? (Y/N): ')
+                    )
+    while (q_response == 'Y' & length(content) > 0)
+        {
+        rand_fact <- noquote(sample(content, 1, replace = FALSE))
+        print(rand_fact)
+        i <- match(rand_fact, content)
+        content <- content[-i]
+        q_response <- NULL
+        q_response <-   as.character(readline(
+                        prompt = 'Do you want to hear another fact? (Y/N): ')
+                        )
+        }
+    if (q_response == 'N') 
+        {
+        readline(noquote(paste('Bye ', get_name, '!', sep = '')))
+        .StopQuietly <- function() {
+            opt <- options(show.error.messages = FALSE)
+            on.exit(options(opt))
+            stop()
             }
-        
-        if (q_response == 'N') 
-            {
-            readline(noquote(paste('Bye ', get_name, '!', sep = '')))
-            stop_quietly <- function() {
-                    opt <- options(show.error.messages = FALSE)
-                    on.exit(options(opt))
-                    stop()
-                    }
+        }
+    if (length(content) == 0) 
+        {
+        readline(noquote(paste('Sorry, there are no more facts.')))
+        readline(noquote(paste('Bye ', get_name, '!', sep = '')))
+        .StopQuietly <- function() {
+            opt <- options(show.error.messages = FALSE)
+            on.exit(options(opt))
+            stop()
             }
-        if (length(content) == 0) 
-            {
-            readline(noquote(paste('Sorry, there are no more facts.')))
-            readline(noquote(paste('Bye ', get_name, '!', sep = '')))
-            stop_quietly <- function() {
-                    opt <- options(show.error.messages = FALSE)
-                    on.exit(options(opt))
-                    stop()
-                    }
-            }
+        }
 }
